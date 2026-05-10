54e047e8d403429652465eadf109adc6ea6721ec
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-05-10 22:30:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+function mergeCraftDates(emily, oliver) {
+  const merged = [];
+  let i = 0, j = 0;
+
+  while (i < emily.length && j < oliver.length) {
+    if (emily[i] <= oliver[j]) {
+      merged.push(emily[i]);
+      i++;
+    } else {
+      merged.push(oliver[j]);
+      j++;
+    }
+  }
+
+  while (i < emily.length) {
+    merged.push(emily[i]);
+    i++;
+  }
+
+  while (j < oliver.length) {
+    merged.push(oliver[j]);
+    j++;
+  }
+
+  return merged;
+}
+
+// Example
+console.log(mergeCraftDates([3, 7, 12, 20], [1, 5, 8, 18, 25]));
+// Output: [1, 3, 5, 7, 8, 12, 18, 20, 25]
